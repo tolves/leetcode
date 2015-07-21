b80a2cb23bfbe173a98a8c4d64cc0294b8ea5e39
tolves <tov.sharing@gmail.com>
2015-07-21 03:10:07 -0700

    #5 is solved

--- 5.Longest Palindromic Substring.rb
+++ 5.Longest Palindromic Substring.rb
@@ -1,49 +1,53 @@
-// Transform S into T.
-// For example, S = "abba", T = "^#a#b#b#a#$".
-// ^ and $ signs are sentinels appended to each end to avoid bounds checking
-string preProcess(string s) {
-  int n = s.length();
-  if (n == 0) return "^$";
-  string ret = "^";
-  for (int i = 0; i < n; i++)
-    ret += "#" + s.substr(i, 1);
- 
-  ret += "#$";
-  return ret;
-}
- 
-string longestPalindrome(string s) {
-  string T = preProcess(s);
-  int n = T.length();
-  int *P = new int[n];
-  int C = 0, R = 0;
-  for (int i = 1; i < n-1; i++) {
-    int i_mirror = 2*C-i; // equals to i' = C - (i-C)
-    
-    P[i] = (R > i) ? min(R-i, P[i_mirror]) : 0;
-    
-    // Attempt to expand palindrome centered at i
-    while (T[i + 1 + P[i]] == T[i - 1 - P[i]])
-      P[i]++;
- 
-    // If palindrome centered at i expand past R,
-    // adjust center based on expanded palindrome.
-    if (i + P[i] > R) {
-      C = i;
-      R = i + P[i];
-    }
-  }
- 
-  // Find the maximum element in P.
-  int maxLen = 0;
-  int centerIndex = 0;
-  for (int i = 1; i < n-1; i++) {
-    if (P[i] > maxLen) {
-      maxLen = P[i];
-      centerIndex = i;
-    }
-  }
-  delete[] P;
-  
-  return s.substr((centerIndex - 1 - maxLen)/2, maxLen);
-}
+
+# // Transform S into T.
+# // For example, S = "abba", T = "^#a#b#b#a#$".
+# // ^ and $ signs are sentinels appended to each end to avoid bounds checking
+
+def  preProcess(s)
+            n = s.length().to_i
+  if n==0
+    return "^$"
+  end
+  ret = "^"
+  (0..n-1).each do |re|
+    ret += ('#' + s[re].to_s)
+  end
+  ret += '#$'
+  return ret
+end
+
+def longestPalindrome(s)
+  t = preProcess(s).to_s
+  n = t.length().to_i
+  c = 0
+  r = 0
+  p = Array.new(n)
+  (1..n-2).each do |m|
+    i_mirror = (2*c - m).to_i
+    p[m] = (r>m) ? [r-m,p[i_mirror]].min : 0
+#   Attempt to expand palindrome centered at i
+    while t[m + 1 + p[m] ] == t[m -1 - p[m] ] do
+      p[m] += 1
+    end
+#   If palindrome centered at i expand past R,
+#   adjust center based on expanded palindrome.
+    if (m + p[m] > r)
+      c = m
+      r = m + p[m]
+    end
+
+  end
+#   Find the maximum element in P.
+  maxLen = 0
+  centerIndex = 0
+  (1..n-2).each do |m|
+    if(p[m] > maxLen)
+      maxLen = p[m]
+      centerIndex = m
+    end
+  end
+  return s[(centerIndex - 1 - maxLen)/2, maxLen];
+
+end
+
+puts longestPalindrome('asdsf')
